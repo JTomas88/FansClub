0bfdf26d8b937e4a8e0752293c4559526171fe7c
Juan Tomas <juan.tomas88@hotmail.com>
2024-12-20 14:14:41 +0100

    cambio url

--- frontend/src/store/flux.js
+++ frontend/src/store/flux.js
@@ -3,7 +3,7 @@ import { parse, format } from 'date-fns';
 const getState = ({ getStore, getActions, setStore }) => {
     return {
         store: {
-            backendUrl: 'http://127.0.0.1:5000', // URL del backend
+            backendUrl: 'https://fans-club.onrender.com', // URL del backend
 
             error: null,
 
