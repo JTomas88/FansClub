fcf71f15591f867a140418d84a185706fe2ddb81
Juan Tomas <juan.tomas88@hotmail.com>
2025-01-31 20:45:28 +0100

    cambios2

--- backend/start.sh
+++ backend/start.sh
@@ -7,7 +7,7 @@ flask db upgrade
 # Asegúrate de que las variables de entorno están configuradas
 export FLASK_APP=api.app  # El archivo Flask se encuentra en backend/api/app.py
 export FLASK_ENV=production
-export DATABASE_URL=postgresql://sienna_admin:Bp3XK6K3GfIEIDcYFPvsNo5hvH09KomD@dpg-cti8otdds78s73be20ag-a.oregon-postgres.render.com/sienna_db
+export DATABASE_URL=postgresql://admin:Bquzhi1fCz6qnPOmnP23f5PzXph3ndeH@dpg-cueho63qf0us73ddirug-a.oregon-postgres.render.com/bdsienna_xett
 
 # Inicia el servidor con Gunicorn (sin flask run)
 gunicorn --bind 0.0.0.0:$PORT api.app:app
