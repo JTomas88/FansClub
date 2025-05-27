fd323a7a799beb6cd5edb8649b1554844365b487
Juan Tomas <juan.tomas88@hotmail.com>
2025-05-27 14:21:53 +0200

    conexiones bd

--- backend/api/test_db_connection.py
+++ backend/api/test_db_connection.py
@@ -1,25 +1,18 @@
+from sqlalchemy import create_engine
 import os
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
-from sqlalchemy.exc import OperationalError
+from sqlalchemy import text
 
-# Carga el .env que está en la misma carpeta que este script
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+load_dotenv()
 
-database_url = os.getenv('DATABASE_URL')
+database_url = os.getenv("DATABASE_URL")
 
-print("DATABASE_URL =", database_url)  # Para verificar que se carga bien
-
-if not database_url:
-    print("No se encontró DATABASE_URL en las variables de entorno")
-    exit(1)
+engine = create_engine(database_url)
 
 try:
-    engine = create_engine(database_url, connect_args={"sslmode": "disable"})
     with engine.connect() as connection:
-        result = connection.execute("SELECT version();")
+        result = connection.execute(text("SELECT NOW();"))
         version = result.fetchone()
-        print(f"Conexión exitosa a la base de datos. Versión de PostgreSQL: {version[0]}")
-except OperationalError as e:
-    print(f"Error en la conexión a la base de datos:\n{e}")
+        print(f"Conexión exitosa a la base de datos. Hora actual: {version[0]}")
+except Exception as e:
+    print("Error de conexión:", e)
