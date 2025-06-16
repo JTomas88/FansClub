676b39179c4d683204520fb221ba872bb149637f
Juan Tomas <juan.tomas88@hotmail.com>
2025-06-16 12:05:18 +0200

    api/sitemap.js

---
+++ api/sitemap.js
@@ -0,0 +1,31 @@
+export default function handler(req, res) {
+    res.setHeader("Content-Type", "application/xml");
+    res.status(200).send(`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://www.siennafans.club/</loc>
+    <lastmod>2025-06-16</lastmod>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://www.siennafans.club/quienessienna</loc>
+    <lastmod>2025-06-16</lastmod>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://www.siennafans.club/objetivoscf</loc>
+    <lastmod>2025-06-16</lastmod>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://www.siennafans.club/links</loc>
+    <lastmod>2025-06-16</lastmod>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://www.siennafans.club/contacto</loc>
+    <lastmod>2025-06-16</lastmod>
+    <priority>0.8</priority>
+  </url>
+</urlset>`);
+}
