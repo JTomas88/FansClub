c6d923ae9f6eccf889e5a814f9a90acac5fe7705
Juan Tomas <juan.tomas88@hotmail.com>
2025-06-25 10:37:43 +0200

    sitemap

--- frontend/api/sitemap.js
+++ frontend/api/sitemap.js
@@ -1,30 +1,32 @@
 export default function handler(req, res) {
-    res.setHeader("Content-Type", "application/xml");
-    res.status(200).send(`<?xml version="1.0" encoding="UTF-8"?>
+  const lastmod = new Date().toISOString().split('T')[0]; // YYYY-MM-DD
+
+  res.setHeader("Content-Type", "application/xml");
+  res.status(200).send(`<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://www.siennafans.club/</loc>
-    <lastmod>2025-06-16</lastmod>
+    <lastmod>${lastmod}</lastmod>
     <priority>1.0</priority>
   </url>
   <url>
     <loc>https://www.siennafans.club/quienessienna</loc>
-    <lastmod>2025-06-16</lastmod>
+    <lastmod>${lastmod}</lastmod>
     <priority>0.8</priority>
   </url>
   <url>
     <loc>https://www.siennafans.club/objetivoscf</loc>
-    <lastmod>2025-06-16</lastmod>
+    <lastmod>${lastmod}</lastmod>
     <priority>0.8</priority>
   </url>
   <url>
     <loc>https://www.siennafans.club/links</loc>
-    <lastmod>2025-06-16</lastmod>
+    <lastmod>${lastmod}</lastmod>
     <priority>0.8</priority>
   </url>
   <url>
     <loc>https://www.siennafans.club/contacto</loc>
-    <lastmod>2025-06-16</lastmod>
+    <lastmod>${lastmod}</lastmod>
     <priority>0.8</priority>
   </url>
 </urlset>`);
